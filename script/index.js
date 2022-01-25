8706d39a96961e395de8d441e22925c73bebe573
Csaba Sipocz <glinto@gmail.com>
2022-01-25 13:34:10 +0100

    create behavior class for the page

---
+++ script/index.js
@@ -0,0 +1,52 @@
+import ScrollSpy from "./scrollspy.js";
+
+
+
+export default class VizzuDocumentation {
+
+	constructor(context) {
+		this.context = context;
+		this.scrollSpy = new ScrollSpy(this.context);
+
+		document.addEventListener("DOMContentLoaded", (event) => {
+			document.querySelector('.action-sidebar-toogle').addEventListener('click', (event) => {
+				this.toggleSideBar();
+				event.preventDefault();
+			});
+			document.querySelector('.vizzu-sidebar-scrim').addEventListener('click', (event) => {
+				this.closeSideBar();
+				event.preventDefault();
+			});
+			/* When we click a link on the sidebar, it will close the sidebar (on mobiles) */
+			document.querySelectorAll('.vizzu-sidebar a[href]').forEach((element) => {
+				element.addEventListener('click', (event) => {
+					this.closeSideBar();
+				});
+			});
+			/* Static thumbnails */
+			document.querySelectorAll('img.thumbnail-static').forEach((element) => {
+				element.addEventListener('click', (event) => {
+					this.loadStaticExample(event.target.dataset.target);
+				});
+			});
+		});
+	}
+
+
+	toggleSideBar() {
+		document.querySelector('.vizzu-page').classList.toggle('vizzu-page-sidebar-toggled');
+	}
+
+	closeSideBar() {
+		document.querySelector('.vizzu-page').classList.remove('vizzu-page-sidebar-toggled');
+	}
+
+	loadStaticExample(url) {
+		console.log(url);
+	}
+
+}
+
+
+
+
