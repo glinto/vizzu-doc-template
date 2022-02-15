468dfb7d9bedaa278485e18352e68b48462a6698
Csaba Sipocz <glinto@gmail.com>
2022-02-15 09:03:29 +0100

    properly reset charts in each section

--- script/snippet-registry.js
+++ script/snippet-registry.js
@@ -71,6 +71,8 @@ class SnippetRegistry {
 
 		// Wait for the cahrt to be initialized
 		await this.snippetPlayerChart.chart.initializing;
+		// Set the base state for the section
+		await this.resetChart(major);
 
 		for (let minor = minorFrom;minor <= minorTo;minor++) {
 			if (this.snippets[major][minor] === undefined) continue;
@@ -95,6 +97,117 @@ class SnippetRegistry {
 
 	}
 
+	resetChart(major) {
+		if (major <= 2) return base.initChart0(this.snippetPlayerChart.chart);
+		if (major > 2 && major <= 5) return base.initChart1(this.snippetPlayerChart.chart);;
+		return base.initChart2(this.snippetPlayerChart.chart);;
+	}
+
 }
+
+function defaultChannel(set = null) {
+	return { set, range: { min: 'auto', max: 'auto' } }
+}
+
+function defaultConfig(channels) {
+	return {
+		data: { filter: null },
+		config: {
+			channels,
+			legend: 'auto',
+			coordSystem: 'cartesian',
+			geometry: 'rectangle',
+			orientation: 'horizontal',
+			sort: 'none',
+			reverse: false,
+			align: 'none',
+			split: false
+		},
+		style: {
+			fontSize: '.90em',
+			plot: {
+				backgroundColor: null,
+				paddingTop: 30,
+				paddingBottom: 25,
+				paddingLeft: null,
+				paddingRight: null,
+				marker: null,
+				xAxis: null,
+				yAxis: null
+			},
+			title: {
+				fontSize: '1.5em',
+				backgroundColor: null,
+				paddingBottom: 0,
+				paddingTop: null,
+				paddingLeft: null,
+				paddingRight: null
+
+			},
+			legend: null,
+			tooltip: null
+		}
+	};
+}
+
+function clearEvent(chart, name, prop) {
+	try {
+		chart.off(name, window[prop]);
+	}
+	catch (e) { }
+}
+
+function clearEvents(chart) {
+	chart.feature('tooltip', false);
+	clearEvent(chart, 'logo-draw', 'logoDrawHandler');
+	clearEvent(chart, 'plot-axis-label-draw', 'labelDrawHandler');
+	clearEvent(chart, 'click', 'clickHandler');
+}
+
+const base = {
+
+	initChart0: chart => {
+		clearEvents(chart);
+		return chart.animate(defaultConfig(
+			{
+				y: defaultChannel(),
+				x: defaultChannel(),
+				color: defaultChannel(),
+				lightness: defaultChannel(),
+				label: defaultChannel(),
+				size: defaultChannel(),
+				noop: defaultChannel()
+			}), '10ms');
+	},
+
+	initChart1: chart => {
+		clearEvents(chart);
+		return chart.animate(defaultConfig(
+			{
+				y: defaultChannel('Popularity'),
+				x: defaultChannel('Genres'),
+				color: defaultChannel(),
+				lightness: defaultChannel(),
+				label: defaultChannel(),
+				size: defaultChannel(),
+				noop: defaultChannel()
+			}), '10ms');
+	},
+
+	initChart2: chart => {
+		clearEvents(chart);
+		chart.feature('tooltip', false);
+		return chart.animate(defaultConfig(
+			{
+				y: defaultChannel(['Popularity', 'Types']),
+				x: defaultChannel('Genres'),
+				color: defaultChannel('Types'),
+				lightness: defaultChannel(),
+				label: defaultChannel('Popularity'),
+				size: defaultChannel(),
+				noop: defaultChannel()
+			}), '10ms');
+	}
+};
 
 registry = new SnippetRegistry();
